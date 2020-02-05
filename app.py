2cfa9756f35a0e7927d1606fda8c03132ec7b5b4
yeboah326 <asanteg36@gmail.com>
2020-02-05 09:08:46 +0000

    Modifications and additions

---
+++ app.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+from flask import Flask, render_template
+from datetime import date
+
+
+app = Flask(__name__)
+today = str(date.today())
+
+@app.route('/')
+
+def index():
+    return render_template("list.html", today = today)
+
+
+if __name__ == "__main__":
+    # ---------Today's Date------------
+
+    app.run(debug=True, use_reloader=True)
